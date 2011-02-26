a9728333a5d8f03e94fbb554cdc9cf5e9e514eec
Andy <demerest@gmail.com>
2011-02-26 17:55:23 +0500

    Added files declarations to manifest.rb

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,8 +1,38 @@
 description "Special kit that allows you create Rails 3 apps quick"
 
+# application layout
+file 'rails/app/views/layouts/application.html.erb', :to => 'app/views/layouts/application.html.erb'
 
+# rails stylesheets
+stylesheet 'rails/public/stylesheets/layout.scss'
+stylesheet 'rails/public/stylesheets/wysiwyg.scss'
+stylesheet 'rails/public/stylesheets/screen.scss', :media => 'screen, projection'
+stylesheet 'rails/public/stylesheets/print.scss',  :media => 'print'
 
-# need to remove
+# playmo additional styles and files
+stylesheet 'playmo/playmo.scss'
+stylesheet 'playmo/article.scss'
+image      'playmo/icons/outgoing.png'
+
+# boilerplate stuff
+html 'boilerplate/files/crossdomain.xml',      :to => 'crossdomain.xml'
+html 'boilerplate/files/robots.txt',           :to => 'robots.txt'
+html 'boilerplate/files/apple-touch-icon.png', :to => 'apple-touch-icon.png'
+html 'boilerplate/files/favicon.ico',          :to => 'favicon.ico'
+
+stylesheet 'boilerplate/css/style.scss',    :media => 'all'
+stylesheet 'boilerplate/css/handheld.scss', :media => 'handheld'
+
+javascript 'boilerplate/js/libs/dd_belatedpng.js',     :to => 'lib/dd_belatedpng.js'
+javascript 'boilerplate/js/libs/jquery-1.4.4.min.js',  :to => 'lib/jquery.min.js'
+javascript 'boilerplate/js/libs/modernizr-1.6.min.js', :to => 'lib/modernizr.min.js'
+javascript 'boilerplate/js/plugins.js',                :to => 'plugins.js'
+javascript 'boilerplate/js/rails.js',                  :to => 'rails.js'
+
+# google analytics config
+file 'google/google.yml', :to => 'config/google.yml'
+
+# need to remove (при помощи rails g playmo:install, rails g playmo:uninstall)
 # public/404.html
 # public/422.html
 # public/500.html
@@ -12,33 +42,6 @@ description "Special kit that allows you create Rails 3 apps quick"
 # public/effects.js
 # public/prototype.js
 # public/rail.js => mootools-ujs or jquery-ujs
-
-=begin
-stylesheet 'style.scss',    :media => 'all'
-stylesheet 'handheld.scss', :media => 'handheld'
-stylesheet 'partials/_base.scss'
-stylesheet 'partials/_example.scss'
-stylesheet 'partials/_fonts.scss'
-stylesheet 'partials/_media.scss'
-stylesheet 'partials/_page.scss'
-
-
-file 'application.html.erb', :to => 'app/views/layouts/application.html.erb'
-file 'files/google.yml',     :to => 'config/google.yml'
-
-javascript 'javascripts/dd_belatedpng.js',     :to => 'lib/dd_belatedpng.js'
-javascript 'javascripts/jquery-1.4.4.min.js',  :to => 'lib/jquery.min.js'
-javascript 'javascripts/modernizr-1.6.min.js', :to => 'lib/modernizr.min.js'
-javascript 'javascripts/plugins.js',           :to => 'plugins.js'
-javascript 'javascripts/rails.js',             :to => 'rails.js'
-
-html 'files/htaccess',             :to => '.htaccess'
-html 'files/crossdomain.xml',      :to => 'crossdomain.xml'
-html 'files/robots.txt',           :to => 'robots.txt'
-html 'files/humans.txt',           :to => 'humans.txt'
-html 'files/apple-touch-icon.png', :to => 'apple-touch-icon.png'
-html 'files/favicon.ico',          :to => 'favicon.ico'
-=end
 
 help %Q{
 

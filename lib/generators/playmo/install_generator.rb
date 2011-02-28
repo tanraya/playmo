ca6b12fdcda1cdb6c28206276e6412b40a8efa66
RubberRails <demerest@gmail.com>
2011-02-28 13:48:06 +0500

    Added home controller generating

--- lib/generators/playmo/install_generator.rb
+++ lib/generators/playmo/install_generator.rb
@@ -8,11 +8,13 @@ module Playmo
       desc "Creates a Playmo initializer and copy locale files to your application."
       source_root File.expand_path('../templates', __FILE__)
 
-      def copy_files
-        template "application.html.erb", "app/views/layouts/application.html.erb"
+      def generate_controller#RAILS_ROOT.split('/').last
+	Rails::Generators.invoke("controller", ['home', 'index'])
       end
-
+      
       def remove_rails_files
+	remove_file 'app/views/layouts/application.html.erb'
+	remove_file 'config/routes.rb'
         remove_file 'public/404.html'
         remove_file 'public/422.html'
         remove_file 'public/500.html'
@@ -22,15 +24,24 @@ module Playmo
         remove_file 'public/effects.js'
         remove_file 'public/prototype.js'
       end
+      
+      def copy_files
+        template "application.html.erb", "app/views/layouts/application.html.erb"
+	template "routes.rb", "config/routes.rb"
+      end
 
       def init_compass
         # TODO rewrite this
-        args = %w{init rails -r playmo-rails -u playmo-rails --sass-dir=public/stylesheets --css-dir=public/stylesheets/compiled --force --trace}
+        args = %w{init rails -r playmo-rails -u playmo-rails --sass-dir=public/stylesheets --css-dir=public/stylesheets/compiled --quiet --force --trace}
         command_line_class = Compass::Exec::Helpers.select_appropriate_command_line_ui(args)
         command_line_class.new(args).run!
       end
 
       #Rails::Generators.invoke("ead_document", [@document_type.table_name.classify, "--document_type_id=#{@document_type.id}"])
+    private  
+      def app_name  
+	RAILS_ROOT.split('/').last.capitalize
+      end  
     end
   end
 end
---
+++ lib/generators/playmo/templates/routes.rb
@@ -0,0 +1,6 @@
+<%= app_name %>::Application.routes.draw do
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+  
+  root :to => "home#index"
+end

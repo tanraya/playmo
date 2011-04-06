67f5c7bcf1b44f6a75893df547c7100e939ae400
RubberRails <demerest@gmail.com>
2011-04-06 12:55:01 +0600

    Playmo helpers upgraded

--- lib/app/helpers/playmo_helper.rb
+++ lib/app/helpers/playmo_helper.rb
@@ -3,15 +3,22 @@ module PlaymoHelper
   # http://paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither
   def ie_tag(name=:body, attrs={}, &block)
     attrs.symbolize_keys!
-    haml_concat("<!--[if lt IE 7 ]> #{ tag(name, add_class('ie6', attrs), true) } <![endif]-->".html_safe)
-    haml_concat("<!--[if IE 7 ]>    #{ tag(name, add_class('ie7', attrs), true) } <![endif]-->".html_safe)
-    haml_concat("<!--[if IE 8 ]>    #{ tag(name, add_class('ie8', attrs), true) } <![endif]-->".html_safe)
-    haml_concat("<!--[if IE 9 ]>    #{ tag(name, add_class('ie9', attrs), true) } <![endif]-->".html_safe)
-    haml_concat("<!--[if (gte IE 9)|!(IE)]><!-->".html_safe)
-    haml_tag name, attrs do
-      haml_concat("<!--<![endif]-->".html_safe)
-      block.call
+    result  = "<!--[if lt IE 7 ]> #{ tag(name, add_class('ie6', attrs), true) } <![endif]-->\n".html_safe
+    result += "<!--[if IE 7 ]>    #{ tag(name, add_class('ie7', attrs), true) } <![endif]-->\n".html_safe
+    result += "<!--[if IE 8 ]>    #{ tag(name, add_class('ie8', attrs), true) } <![endif]-->\n".html_safe
+    result += "<!--[if IE 9 ]>    #{ tag(name, add_class('ie9', attrs), true) } <![endif]-->\n".html_safe
+    result += "<!--[if (gte IE 9)|!(IE)]><!-->".html_safe
+    
+    
+    #tag name, attrs do
+    #  haml_concat("<!--<![endif]-->".html_safe)
+    #  block.call
+    #end
+    result += content_tag name, attrs do
+      "<!--<![endif]-->\n".html_safe + with_output_buffer(&block)
     end
+    
+    result
   end
 
   def ie_html(attrs={}, &block)

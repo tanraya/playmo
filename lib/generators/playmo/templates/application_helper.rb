eb788155f023921505318bd920499c879f58d22f
RubberRails <demerest@gmail.com>
2011-04-21 18:09:22 +0600

    Added some useful helpers

--- lib/generators/playmo/templates/application_helper.rb
+++ lib/generators/playmo/templates/application_helper.rb
@@ -18,4 +18,35 @@ module ApplicationHelper
     title(heading)
     content_tag tag, heading
   end
+
+  def admin_area(&block)
+    if user_signed_in?
+      content = with_output_buffer(&block)
+      content_tag(:div, content, :class => 'admin')
+    end
+  end
+
+  def link_to_section(name, options = {}, html_options = {}, &block)
+    url_string = url_for(options)
+
+    if "/#{request.path.split('/')[1]}" == url_string
+      html_options[:class] = "#{html_options[:class]} current"
+    end
+
+    link_to(name, options, html_options, &block)
+  end
+
+  def page_id
+    name = 'page-' + request.path_parameters[:controller] + '-' + request.path_parameters[:action]
+    name.gsub!(/_+/, '-')
+    name
+  end
+
+  def link_to_website(url, html_options = {})
+    return nil if url.blank?
+    
+    url = "http://#{url}" unless url =~ /^(ht|f)tps?:\/\//i
+    html_options[:href] = url
+    content_tag(:a, url, html_options)
+  end
 end

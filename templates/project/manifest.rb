2fb979bd1bab326767548a9df646b2273dfedc76
RubberRails <demerest@gmail.com>
2011-04-22 13:45:26 +0600

    Big changes

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,15 +1,15 @@
 description "Special kit that allows you create Rails 3 apps quick"
 
 # rails stylesheets
-stylesheet 'rails/public/stylesheets/layout.scss', :to => 'rails/layout.scss'
-stylesheet 'rails/public/stylesheets/screen.scss', :to => 'rails/screen.scss', :media => 'screen, projection'
-stylesheet 'rails/public/stylesheets/print.scss', :to => 'rails/print.scss',  :media => 'print'
-stylesheet 'rails/public/stylesheets/wysiwyg.scss', :to => 'rails/wysiwyg.scss'
+#stylesheet 'rails/public/stylesheets/layout.scss',  :to => 'layout.scss'
+stylesheet 'rails/public/stylesheets/screen.scss',  :to => 'screen.scss', :media => 'screen, projection'
+#stylesheet 'rails/public/stylesheets/print.scss',   :to => 'print.scss',  :media => 'print'
+#stylesheet 'rails/public/stylesheets/wysiwyg.scss', :to => 'wysiwyg.scss'
 
 # playmo additional styles and files
-stylesheet 'playmo/playmo.scss'
-stylesheet 'playmo/article.scss'
-image      'playmo/icons/outgoing.png'
+#stylesheet 'playmo/playmo.scss' <-- move into rails screen on top
+#stylesheet 'playmo/article.scss'
+#image      'playmo/icons/outgoing.png'
 
 # boilerplate stuff
 html 'boilerplate/files/crossdomain.xml',      :to => 'crossdomain.xml'
@@ -17,7 +17,7 @@ html 'boilerplate/files/robots.txt',           :to => 'robots.txt'
 html 'boilerplate/files/apple-touch-icon.png', :to => 'apple-touch-icon.png'
 html 'boilerplate/files/favicon.ico',          :to => 'favicon.ico'
 
-stylesheet 'boilerplate/css/style.scss',    :media => 'all'
+#stylesheet 'boilerplate/css/style.scss',    :media => 'all'
 
 javascript 'boilerplate/js/libs/dd_belatedpng.js',     :to => 'lib/dd_belatedpng.js'
 javascript 'boilerplate/js/libs/modernizr-1.7.min.js', :to => 'lib/modernizr-1.7.min.js'

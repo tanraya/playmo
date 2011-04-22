d615ff9e98aa0c9a8dfe253e22eb7c1b834e3d10
RubberRails <demerest@gmail.com>
2011-04-22 14:19:10 +0600

    Big changes

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,15 +1,16 @@
 description "Special kit that allows you create Rails 3 apps quick"
 
 # rails stylesheets
-#stylesheet 'rails/public/stylesheets/layout.scss',  :to => 'layout.scss' <-- this is partial!
-stylesheet 'rails/public/stylesheets/screen.scss',  :to => 'screen.scss', :media => 'screen, projection'
-#stylesheet 'rails/public/stylesheets/print.scss',   :to => 'print.scss',  :media => 'print'
-#stylesheet 'rails/public/stylesheets/wysiwyg.scss', :to => 'wysiwyg.scss' <-- wysiwyg_screen 
+stylesheet 'rails/public/stylesheets/partials/_constants.scss', :to => 'partials/_constants.scss'
+stylesheet 'rails/public/stylesheets/partials/_layout.scss',    :to => 'partials/_layout.scss'
+stylesheet 'rails/public/stylesheets/partials/_overrides.scss', :to => 'partials/_overrides.scss'
+stylesheet 'rails/public/stylesheets/partials/_article.scss',   :to => 'partials/_article.scss'
+stylesheet 'rails/public/stylesheets/screen.scss',              :to => 'screen.scss'
+stylesheet 'rails/public/stylesheets/media.scss',               :to => 'media.scss'
+stylesheet 'rails/public/stylesheets/wysiwyg_screen.scss',      :to => 'wysiwyg_screen.scss'
 
-# playmo additional styles and files
-#stylesheet 'playmo/playmo.scss' <-- move into rails screen on top
-#stylesheet 'playmo/article.scss'
-#image      'playmo/icons/outgoing.png'
+# additional files
+image      'images/icons/outgoing.png'
 
 # boilerplate stuff
 html 'boilerplate/files/crossdomain.xml',      :to => 'crossdomain.xml'
@@ -17,7 +18,6 @@ html 'boilerplate/files/robots.txt',           :to => 'robots.txt'
 html 'boilerplate/files/apple-touch-icon.png', :to => 'apple-touch-icon.png'
 html 'boilerplate/files/favicon.ico',          :to => 'favicon.ico'
 
-#stylesheet 'boilerplate/css/style.scss',    :media => 'all'
 
 javascript 'boilerplate/js/libs/dd_belatedpng.js',     :to => 'lib/dd_belatedpng.js'
 javascript 'boilerplate/js/libs/modernizr-1.7.min.js', :to => 'lib/modernizr-1.7.min.js'

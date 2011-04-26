01864c3f19cdc08597cb1e394888a071699bfcb8
RubberRails <demerest@gmail.com>
2011-04-26 19:16:51 +0600

    Experiments with templates

--- lib/generators/playmo/layout_generator.rb
+++
@@ -1,9 +0,0 @@
-module Playmo
-  module Generators
-    class LayoutGenerator < Rails::Generators::Base
-      desc "Generates layouts for your application."
-      source_root File.expand_path('../templates', __FILE__)  
-      argument :layout_name, :type => :string, :default => "application"  
-    end
-  end
-end  
--- lib/generators/rails/playmo_controller_generator.rb
+++
@@ -1,9 +0,0 @@
-require 'rails/generators/rails/scaffold_controller/scaffold_controller_generator'
-
-module Rails
-  module Generators
-    class PlaymoControllerGenerator < ScaffoldControllerGenerator
-      source_root File.expand_path("../templates", __FILE__)
-    end
-  end
-end

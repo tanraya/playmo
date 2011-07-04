224d12e24e75d57a65b99e0be11bd2a91a3e5a5f
tanraya <demerest@gmail.com>
2011-07-05 00:18:09 +0600

    Added JavascriptFramework installers with 3 most popular frameworks

--- lib/generators/playmo/installers/javascript_framework.rb
+++ lib/generators/playmo/installers/javascript_framework.rb
@@ -2,34 +2,29 @@ module Playmo
   module Generators
     module Installers
       class JavascriptFramework < Base
-        # Если вопрос без вариантов ответов - генерить (y/n), в
-        # противном случае - (1, 2 or 3)
-
         def setup
           question "Please choose JS framework you prefer to install" do
-            answer "JQuery 1.5.2 (with Jquery UI)"   => :install_jquery
-            answer "Mootools Core 1.3.1 (with More)" => :install_mootools
-            answer "Prototype"                       => :install_prototype
+            answer "JQuery (with Jquery UI)"                => :install_jquery
+            answer "Mootools Core (with More)"              => :install_mootools
+            answer "Prototype (with Scriptaculous and RJS)" => :install_prototype
           end
         end
 
       protected
 
-        # Есть 3 гема с этими фреймворками, но так же нужно добавить ujs для них
-        # Гемы нужно добавлять в Gemfile, а так же удалять оттуда, если они существуют.
-        # Лучше все записывать куда-нить, а потом пачкой генерить/удалять файлы или добавлять гемы,
-        # таким образом будет возможность сгенерировать, например, красивый Gemfile
-
+        # See https://github.com/rails/jquery-rails for detailt
         def install_jquery
           gem "jquery-rails"
         end
 
+        # See https://github.com/neonlex/mootools-rails for detailt
         def install_mootools
-          gem "jquery-mootools"
+          gem 'mootools-rails'
         end
 
+        # See https://github.com/rails/prototype-rails for details
         def install_prototype
-          gem "jquery-prototype"
+          gem "prototype-rails"
         end
       end
     end

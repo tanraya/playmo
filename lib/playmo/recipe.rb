199defaaf946899e06917adbf3ce83ae6e10fb01
Andrew Kozloff <demerest@gmail.com>
2011-09-22 21:54:25 +0600

    Working on Recipe

--- lib/playmo/recipe.rb
+++ lib/playmo/recipe.rb
@@ -3,9 +3,9 @@ require 'rails/generators'
 module Playmo
   # Base class for all recipes
   class Recipe < Rails::Generators::Base
-    #source_root File.expand_path('../templates', __FILE__)
-    attr_accessor :question_instance, :silents
+    #attr_accessor :question_instance, :silents
 
+=begin
     def question(arg, &block)
       @question_instance = Playmo::Question.new(arg, &block)
     end
@@ -13,6 +13,14 @@ module Playmo
     def silently(&block)
       @silents ||= []
       @silents << Playmo::Silent.new(self, &block)
+    end
+=end
+    def question(arg, &block)
+
+    end
+
+    def silently(&block)
+      
     end
   end
 end

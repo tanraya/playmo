e23e4b4f9954ec5919ff7b38e075cc3534c211c5
tanraya <demerest@gmail.com>
2011-10-02 23:34:05 +0600

    Added compass & javascripts recipes

---
+++ lib/generators/playmo/recipes/compass_recipe.rb
@@ -0,0 +1,20 @@
+module Playmo
+  module Generators
+    module Recipes
+      class CompassRecipe < Playmo::Recipe
+        source_root File.expand_path('../templates/compass_recipe', __FILE__)
+
+        def setup
+          question "Would you like to use Compass in this project?" => :install_compass
+        end
+
+        def install_compass
+          gem 'compass'
+        end
+      end
+    end
+  end
+end
+
+# Write down this recipe to our Cookbook if it's available
+Playmo::Cookbook.instance.use(Playmo::Generators::Recipes::CompassRecipe) if defined?(Playmo::Cookbook)

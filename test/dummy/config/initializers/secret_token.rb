71a851323e9956cace6e21e6b77d3b8d2ff5621e
Andrew Kozloff <demerest@gmail.com>
2011-10-29 12:28:02 +0600

    test/ removed

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '6f01d5bb38c94a63800c0c758cf5b45a435240b151d37b063f0ae5ed5ab6a4fb5e074e83a0919944d99bbc891e9e7f2df255d9f444538dfcbda179ea298e3807'
--- test/home_controller_recipe.rb
+++
@@ -1,4 +0,0 @@
-require 'rails/all'
-require File.dirname(__FILE__) + '/../lib/playmo/recipe'
-require File.dirname(__FILE__) + '/../lib/generators/playmo/recipes/home_controller_recipe'
-recipe = Playmo::Generators::Recipes::HomeControllerRecipe.new

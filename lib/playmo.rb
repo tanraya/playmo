3d277969afe93230338b7a34c6344066b2b1b083
tanraya <demerest@gmail.com>
2011-12-25 20:30:53 +0600

    Removed railtie

--- lib/playmo.rb
+++ lib/playmo.rb
@@ -27,7 +27,13 @@ require 'rails/all'
 
 module Playmo
   extend ActiveSupport::Autoload
-  require "#{File.dirname(__FILE__)}/playmo/railtie"
+  
+  class Railtie < Rails::Railtie
+    config.app_generators do |g|
+      path = File::expand_path('../generators/templates', __FILE__)
+      g.templates.unshift path
+    end 
+  end
 
   autoload :Cli
   autoload :Event
--- lib/playmo/railtie.rb
+++
@@ -1,8 +0,0 @@
-module Playmo
-  class Railtie < Rails::Railtie
-    config.app_generators do |g|
-      path = File::expand_path('../generators/templates', __FILE__)
-      g.templates.unshift path
-    end 
-  end
-end 

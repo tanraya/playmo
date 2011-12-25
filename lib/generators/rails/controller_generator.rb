b344b73831b09ae5df8c60c96bd46f83b205367e
tanraya <demerest@gmail.com>
2011-12-25 18:27:31 +0600

    Removed old wrong generators & templates

--- lib/generators/rails/controller_generator.rb
+++
@@ -1,5 +0,0 @@
-require 'rails/generators/rails/controller/controller_generator'
-
-class Rails::Generators::ControllerGenerator
-  (class << self; self; end).class_eval { source_root File.expand_path("../templates", __FILE__) }
-end

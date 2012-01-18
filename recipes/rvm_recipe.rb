befedad86b21802a2f5fd27124934ab802358a0a
tanraya <demerest@gmail.com>
2012-01-18 19:58:12 +0600

    Added sample piece of code into rvm recipe

--- recipes/rvm_recipe.rb
+++ recipes/rvm_recipe.rb
@@ -5,6 +5,7 @@ recipe :rvm do
   silently do
     if system 'which rvm > /dev/null'
       in_root do
+      	#run '[ -s "$HOME/.rvm/scripts/rvm" ] && . "$HOME/.rvm/scripts/rvm"'
         run "rvm #{RUBY_VERSION}@#{application_name} --rvmrc --create"
       end
     end

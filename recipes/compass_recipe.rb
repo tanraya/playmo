cdb2c1b5ae7d622d096776978bbddcee851e8bf2
tanraya <demerest@gmail.com>
2012-02-06 11:35:57 +0600

    Upgrade compass to 0.12.rc.1

--- recipes/compass_recipe.rb
+++ recipes/compass_recipe.rb
@@ -3,6 +3,6 @@ recipe :compass do
   after :application_controller
   
   ask "Would you like to use Compass in this project?" do
-    gem "compass", "~> 0.12.alpha.3"
+    gem "compass", "0.12.rc.1"
   end
 end

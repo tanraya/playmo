0a887e7bfec9de9aa7578a9c17ef69cd28a398e6
tanraya <demerest@gmail.com>
2012-04-17 22:14:49 +0600

    attr_accessible fix for Role model

--- lib/playmo/version.rb
+++ lib/playmo/version.rb
@@ -1,5 +1,5 @@
 module Playmo
-  VERSION = "0.1.9"
+  VERSION = "0.1.10"
 
   if ARGV.first =~ /--version|-v/
     puts VERSION
--- recipes/templates/cancan_recipe/role.rb
+++ recipes/templates/cancan_recipe/role.rb
@@ -3,6 +3,7 @@
 class Role < ActiveRecord::Base
   has_many :users, :through => :user_role
 
+  attr_accessible :name, :description
   validates :name, :description, :presence => true
 
   def to_s
